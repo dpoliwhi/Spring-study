a78b79241ca738d5c9d256f967bbd5c459385f60
didenkoev93 <didenkoev93@gmail.com>
2022-12-03 19:27:08 +0700

    in progress

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,8 +6,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("classpath:beans.xml");
-        MessageProvider provider = context.getBean(MessageProvider.class);
         MessageRenderer renderer = context.getBean(MessageRenderer.class);
         renderer.render();
+
     }
 }
--- src/main/java/org/example/MessageProviderSimple.java
+++ src/main/java/org/example/MessageProviderSimple.java
@@ -1,8 +1,14 @@
 package org.example;
 
 public class MessageProviderSimple implements MessageProvider {
+    private String message;
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
     @Override
     public String getMessage() {
-       return "Happy new year!";
+       return message;
     }
 }
